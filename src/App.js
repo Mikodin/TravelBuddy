63f570a615bf98aeb48512d23db9f348a8fe706e
Mike Alicea <mfalicea58@gmail.com>
2017-11-08 13:01:10 -0500

    Added reactstrap, cleaned up login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import VisibleTransactions from './containers/TransactionsList.C';
+import TransactionsList from './containers/TransactionsList.C';
 import LoginContainer from './containers/Login.C';
 
 class App extends Component {
-  addTrans = () => {
-    console.log('Add trans');
+  constructor(props) {
+    super(props);
+    this.state = {
+      skipLogin: false,
+      isLoggedIn: false,
+    };
+  }
+
+  skipLogin = () => {
+    this.setState({ skipLogin: true });
+  };
+
+  login = isLoggedIn => {
+    this.setState({ isLoggedIn });
   };
 
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-        <VisibleTransactions />
-        <LoginContainer />
-        <button onClick={() => this.addTrans()}>Add trans</button>
+      <div className="container App">
+        {this.state.skipLogin || this.state.isLoggedIn ? (
+          <TransactionsList />
+        ) : (
+          <LoginContainer login={this.login} skipLogin={this.skipLogin} />
+        )}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import './index.css';
 import App from './App';
